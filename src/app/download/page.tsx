9436762c1fbf2793633be257a764f26d442587fa
AJ Hermosillo <rockenu4118@gmail.com>
2024-02-03 04:15:20 -0800

    download

---
+++ src/app/download/page.tsx
@@ -0,0 +1,10 @@
+export default function Download() {
+    return (
+        <main>
+            <h1>Download Keyser</h1>
+            <button className="p-4 border-2">
+                <a href="/Keyser">Linux</a>
+            </button>
+        </main>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <main>
             <h1>Keyser Protocol</h1>
+            <Link href="/download">Download</Link>
         </main>
     );
 }
